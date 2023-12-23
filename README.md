8b59561180888ee6138be412ec9ae27671b4aeee
implodingduck <zsek320@gmail.com>
2023-12-23 13:26:10 -0600

    adding credits and referencing images

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # static
 
 * [https://implodingduck.github.io/static/xmas23/#](https://implodingduck.github.io/static/xmas23/#)
+
+
+## Credits
+* Vite
+* React
+* Games inspired by Connections
+* Image by [Freepik](https://www.freepik.com/free-vector/watercolor-12-days-christmas-illustration_20550713.htm)
--- xmas23/src/Math12.jsx
+++ xmas23/src/Math12.jsx
@@ -14,12 +14,12 @@ function Math12() {
     <>
       <div className="math12">
         <h2>Twelve Days of Math-mas</h2>
-        <p>A = 12 / 3</p>
-        <p>B = 11 * 2</p>
-        <p>C = 10 + 1</p>
-        <p>X = 9 - 4</p>
-        <p>Y = 8 + 6</p>
-        <p>Z = 7 * 5</p>
+        <p>A = <img src="/tdom12.png" /> / <img src="/tdom3.png" /></p>
+        <p>B = <img src="/tdom11.png" /> * <img src="/tdom2.png" /></p>
+        <p>C = <img src="/tdom10.png" /> + <img src="/tdom1.png" /></p>
+        <p>X = <img src="/tdom9.png" /> - <img src="/tdom4.png" /></p>
+        <p>Y = <img src="/tdom8.png" /> + <img src="/tdom6.png" /></p>
+        <p>Z = <img src="/tdom7.png" /> * <img src="/tdom5.png" /></p>
 
         <p>(A + C) * X / ((B + Y) - Z) = ???</p>
       </div>
@@ -30,6 +30,9 @@ function Math12() {
 
       <div>
         <button className="submit" onClick={handleSubmit}>Submit</button>
+      </div>
+      <div className="footer">
+        <p>Image by <a href="https://www.freepik.com/free-vector/watercolor-12-days-christmas-illustration_20550713.htm">Freepik</a></p>
       </div>
     </>
   )
